17c204a91980197cfbcd657ddee31c6da83ae047
byeongyeol <qudduf9313@gmail.com>
2022-07-07 01:05:56 +0900

    Csv file make

--- Quicksort.py
+++ Quicksort.py
@@ -2,9 +2,12 @@ import random
 import pandas as pd
 import csv
 
-Number_list = [1000000]
-for i in Number_list:
-    Random_number = random.randint(1,2147483647)
+
+   
+Number_list = []
+for i in range(1000000):
+    Random_number = random.randint(1,100)
     Number_list.append(Random_number)
-    print(Random_number)
+    random.shuffle(Number_list) # 랜덤 상태
+file = open('random_quicksort.csv', 'w', encoding='utf-8', newline='')
 # CSV 저장하는 것 추가 
