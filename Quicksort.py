198ba56c844c7e6ad6c50cd2565fc370fdb3135f
byeongyeol <qudduf9313@gmail.com>
2022-07-07 10:08:41 +0900

    csv writer add

--- Quicksort.py
+++ Quicksort.py
@@ -10,4 +10,7 @@ for i in range(1000000):
     Number_list.append(Random_number)
     random.shuffle(Number_list) # 랜덤 상태
 file = open('random_quicksort.csv', 'w', encoding='utf-8', newline='')
+csv_writer = csv.writer(file)
+csv_writer.writerows(Number_list)
+
 # CSV 저장하는 것 추가 
