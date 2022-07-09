67de117d04a86c741ad7d5dd0ea50d336f9e61d4
byeongyeol <qudduf9313@gmail.com>
2022-07-09 17:03:27 +0900

    csv writer fix

--- Quicksort.py
+++ Quicksort.py
@@ -6,12 +6,12 @@ import csv
    
 Number_list = []
 for i in range(1000000):
-    Random_number = random.randint(1,100)
-    Number_list.append(Random_number)
+    Random_number = random.randint(1, 100)
     random.shuffle(Number_list) # 랜덤 상태
 file = open('random_quicksort.csv', 'w', encoding='utf-8', newline='')
 csv_writer = csv.writer(file)
 csv_writer.writerows(Number_list)
+file.close()
 #csv 수정
 
 # CSV 저장하는 것 추가 
