04b3b1f692fade739bc80b9a86810f8369d962d7
byeongyeol <qudduf9313@gmail.com>
2022-07-12 21:14:21 +0900

    main quicksort csv fix

--- Quicksort.py
+++ Quicksort.py
@@ -4,17 +4,15 @@ import numpy as np
 import csv
 
 
-   
+
 def Random_number():
     Number_list = []
-    # for i in range(1000000):
-    np.random.seed()
-    Random_number = np.random.randint(1, 100, size = 1000000)
-    print(Random_number)
+    Random_number = []
+    for i in range(1000000):
+        Random_number.append(np.random.randint(1, 100))
+    
     Number_list.append(Random_number)
-    print(Number_list)
     random.shuffle(Number_list) # 랜덤 상태
-    print(Number_list)
     file = open('random_quicksort.csv', 'w', encoding='utf-8', newline='')
     csv_writer = csv.writer(file)
     csv_writer.writerow(Number_list)
@@ -22,4 +20,3 @@ def Random_number():
     #csv 수정
 
     # CSV 저장하는 것 추가 
-
