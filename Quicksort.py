f614f585060c7a2916934723fd42014ff55a47b7
byeongyeol <qudduf9313@gmail.com>
2022-07-12 09:15:34 +0900

    main random spit

--- Quicksort.py
+++ Quicksort.py
@@ -1,17 +1,25 @@
 import random
 import pandas as pd
+import numpy as np
 import csv
 
 
    
-Number_list = []
-for i in range(1000000):
-    Random_number = random.randint(1, 100)
+def Random_number():
+    Number_list = []
+    # for i in range(1000000):
+    np.random.seed()
+    Random_number = np.random.randint(1, 100, size = 1000000)
+    print(Random_number)
+    Number_list.append(Random_number)
+    print(Number_list)
     random.shuffle(Number_list) # 랜덤 상태
-file = open('random_quicksort.csv', 'w', encoding='utf-8', newline='')
-csv_writer = csv.writer(file)
-csv_writer.writerows(Number_list)
-file.close()
-#csv 수정
+    print(Number_list)
+    file = open('random_quicksort.csv', 'w', encoding='utf-8', newline='')
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(Number_list)
+    file.close()
+    #csv 수정
 
-# CSV 저장하는 것 추가 
+    # CSV 저장하는 것 추가 
+
---
+++ main.py
@@ -0,0 +1,4 @@
+import Quicksort
+
+if __name__ =="__main__":
+    Quicksort.Random_number()
