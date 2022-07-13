af12633de8e65c680e0ff88fa59454fa2b5cc56d
byeongyeol <qudduf9313@gmail.com>
2022-07-13 15:09:47 +0900

    make csv and csv sort code finish

--- Quicksort.py
+++ Quicksort.py
@@ -4,19 +4,55 @@ import numpy as np
 import csv
 
 
+def swap(big_value, small_value):
+    temp = big_value
+    big_value = small_value
+    small_value = temp
 
 def Random_number():
-    Number_list = []
-    Random_number = []
-    for i in range(1000000):
-        Random_number.append(np.random.randint(1, 100))
+    Random_list = list(range(1,1000000))
+    random.shuffle(Random_list) # 랜덤 상태
     
-    Number_list.append(Random_number)
-    random.shuffle(Number_list) # 랜덤 상태
     file = open('random_quicksort.csv', 'w', encoding='utf-8', newline='')
     csv_writer = csv.writer(file)
-    csv_writer.writerow(Number_list)
+    csv_writer.writerow(Random_list)
     file.close()
-    #csv 수정
 
-    # CSV 저장하는 것 추가 
+
+
+
+def Forward_sort():
+    Forward_list = list(range(1,1000000))
+    file = open('Forward_sort.csv', 'w', encoding='utf-8', newline='')
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(Forward_list)
+    file.close()
+
+def Reverse_sort():
+    Reverse_list = list(range(1,1000000))
+    Reverse_list.sort(reverse=True)
+    
+    file = open('Reverse_sort.csv', 'w', encoding='utf-8', newline='')
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(Reverse_list)
+    file.close()
+    
+def sort_action():
+    Random_csv = pd.read_csv('random_quicksort.csv')
+    print(Random_csv.to_string())  
+#csv 수정
+#     Number_list = []
+#     Random_number = []
+#     for i in range(1000000):
+#         Random_number.append(np.random.randint(1, 100))
+    
+#     Number_list.append(Random_number)
+#     for end in range(1000000):
+#         #순방향 정렬 구현하였으나 시간이 많이 걸려 주석 처리
+#         # Random_number.append(np.random.randint(1,100))
+#         # for i in range(end, 0, -1):
+#         #     if i>=1:
+#         #         if Random_number[i] < Random_number[i-1]:
+#         #             Random_number[i], Random_number[i-1] = Random_number[i-1], Random_number[i]
+#                 # swap(Random_number[i-1], Random_number(i))
+    
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import Quicksort
+import time
 
 if __name__ =="__main__":
+    start = time.time()
     Quicksort.Random_number()
+    Quicksort.Forward_sort()
+    Quicksort.Reverse_sort()
+    Quicksort.sort_action()
+    end = time.time()
+    print(time.time()-start)
